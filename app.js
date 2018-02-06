61583fcc10125f7357a97cdc0d4b3f9467244e63
4Chazz <chastitylawson1@gmail.com>
2018-02-06 13:09:42 -0600

    Json Library added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const http = new easyHttp;
+const http = new easyHTTP;
 
 // Get Posts
 
-http.get('https://jsonplaceholder.typicode.com/posts');
+http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(posts);
+    }
+});
